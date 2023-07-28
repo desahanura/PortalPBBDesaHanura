3627beda535fa1edf78e5094a7f4c754a0d9e5c1
aniisahms <anisahmufida@gmail.com>
2023-07-28 23:54:34 +0700

    contributor

--- project-kkn/app/Views/viewAboutUs.php
+++ project-kkn/app/Views/viewAboutUs.php
@@ -19,42 +19,41 @@
                     <h4>Contributor</h4>
                 </div>
                 <div class="card-body">
-                    <i>Tim IT KKN Unila Periode II Tahun 2023 Desa Hanura</i>
-                    <!-- foto -->
-                    <div class="row">
-                        <div class="col">
-                            <img src="images/contributor/contributor_aniisah.png" alt="contributor" style="max-width: 200px;">
-                        </div>
-                        <div class="col">
-                            <img src="images/contributor/contributor_aniisah.png" alt="contributor" style="max-width: 200px;">
-                        </div>
-                        <div class="col">
-                            <img src="images/contributor/contributor_aniisah.png" alt="contributor" style="max-width: 200px;">
+                    <div style="margin-bottom: 10px;">
+                        <b>Version 1.0</b><br>
+                        <a>Tim IT KKN Unila Periode II Tahun 2023 Desa Hanura</a>
+                        <br><br>
+                        <i>- 26 Juli 2023 s.d. 4 Agustus 2023 -</i>
+                    </div>
+                    <div style="justify-content: space-between;">
+                        <!-- biodata -->
+                        <div class="row">
+                            <div class="col card" style="margin: 20px; align-items: center; padding: 30px; border-radius: 10px;">
+                                <img src="images/contributor/contributor_aniisah_circle.png" alt="contributor" style="max-width: 200px; margin-bottom: 30px;">
+                                <b style="margin-bottom: 20px; color: #6777ef">Aniisah Mufiidah Suharso</b>
+                                <a>2017051012</a>
+                                <a>S1 Ilmu Komputer</a>
+                                <a>FMIPA Universitas Lampung</a>
+                            </div>
+                            <div class="col card" style="margin: 20px; align-items: center; padding: 30px; border-radius: 10px;">
+                                <img src="images/contributor/contributor_ages_circle.png" alt="contributor" style="max-width: 200px; margin-bottom: 30px;">
+                                <b style="margin-bottom: 20px; color: #6777ef">Ages Mahesa</b>
+                                <a>2017051027</a>
+                                <a>S1 Ilmu Komputer</a>
+                                <a>FMIPA Universitas Lampung</a>
+                            </div>
+                            <div class="col card" style="margin: 20px; align-items: center; padding: 30px; border-radius: 10px;">
+                                <img src="images/contributor/contributor_aura_circle.png" alt="contributor" style="max-width: 200px; margin-bottom: 30px;">
+                                <b style="margin-bottom: 20px; color: #6777ef">Aura Husnaini Putri Zaidani</b>
+                                <a>2017051045</a>
+                                <a>S1 Ilmu Komputer</a>
+                                <a>FMIPA Universitas Lampung</a>
+                            </div>
                         </div>
                     </div>
-                    <!-- nama -->
-                    <div class="row">
-                        <div class="col">
-                            <div class="mb-2">Aniisah Mufiidah Suharso</div>
-                        </div>
-                        <div class="col">
-                            <div class="mb-2">Ages Mahesa</div>
-                        </div>
-                        <div class="col">
-                            <div class="mb-2">Aura Husnaini Putri Zaidani</div>
-                        </div>
-                    </div>
-                    <!-- npm -->
-                    <div class="row">
-                        <div class="col">
-                            <div class="mb-2">2017051012</div>
-                        </div>
-                        <div class="col">
-                            <div class="mb-2">2017051027</div>
-                        </div>
-                        <div class="col">
-                            <div class="mb-2">2017051045</div>
-                        </div>
+                    <!-- fitur yang dikembangkan -->
+                    <div style="margin-top: 10px;">
+                        <b>Fitur yang Dikembangkan</b>
                     </div>
                 </div>
             </div>
